5fc9a93fdade90fd3fb8a243cf9b42c50b916ae3
Yana <yanochkalaz0560@gmail.com>
2024-03-06 15:13:14 +0200

    Updates

---
+++ assets/index-lO3UziG9.js
@@ -0,0 +1,1 @@
+import{h as e,i as a,j as s}from"./index-CrSLo1wP.js";import{P as o}from"./index-B-nwKBhL.js";import{T as r}from"./index-b4De2d16.js";const l=()=>{const t=e(a);return s.jsx(o,{title:"Tasks",children:s.jsx(r,{tasks:t})})};export{l as default};
